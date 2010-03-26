eb427eeb5b241e95bfe08cac8777adb2819a856f
Christopher X. Candreva <chris@westnet.com>
2010-03-26 23:15:11 +0000

    Update version/author information in pnversion.php

--- pnversion.php
+++ pnversion.php
@@ -1,40 +1,25 @@
 <?php
-// $Id: pnversion.php,v 1.2 2002/06/29 10:38:59 philip Exp $
-// ----------------------------------------------------------------------
-// FormExpress module for POST-NUKE Content Management System
-// Copyright (C) 2002 by Stutchbury Limited
-// http://www.stutchbury.net/
-// ----------------------------------------------------------------------
-// Based on:
-// PHP-NUKE Web Portal System - http://phpnuke.org/
-// Thatware - http://thatware.org/
-// ----------------------------------------------------------------------
-// LICENSE
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WIthOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
-// Original Author of file: Philip Fletcher
-//
+/**
+ * FormExpress : Build forms for Zikula through a web interface
+ *
+ * @copyright (c) 2002 Stutchbury Limited, 2010 Chris Candreva
+ * @Version $Id:                                              $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package FormExpress
+ *
+ */
+
 $modversion['name'] = 'FormExpress';
-$modversion['version'] = '0.3.5';
+$modversion['displayname'] = 'FormExpress';
 $modversion['description'] = 'Admin configurable Input Forms';
+$modversion['version'] = '0.4.0';
 $modversion['credits'] = 'docs/credits.txt';
 $modversion['help'] = 'docs/help.txt';
 $modversion['changelog'] = 'docs/changelog.txt';
 $modversion['license'] = 'docs/license.txt';
 $modversion['official'] = 0;
-$modversion['author'] = 'Philip Fletcher';
-$modversion['contact'] = 'http://www.stutchbury.net/';
+$modversion['author'] = 'Philip Fletcher, Chris Candreva';
+$modversion['contact'] = 'http://code.zikula.org/formexpress/';
 $modversion['admin'] = 1;
 $modversion['securityschema'] = array('FormExpress::' => 'Block title::');
 ?>

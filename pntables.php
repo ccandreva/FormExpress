521b16cddf3cbaf3e528c1236ce419a726848977
Christopher X. Candreva <chris@westnet.com>
2011-02-25 20:20:51 +0000

    Convert pntables to DBUtil format

--- pntables.php
+++ pntables.php
@@ -1,31 +1,33 @@
 <?php
-// $Id$
-// ----------------------------------------------------------------------
-// FormExpress module for POST-NUKE Content Management System
-// Copyright (C) 2002 by Stutchbury Limited
-// http://www.stutchbury.net/
-// ----------------------------------------------------------------------
-// Based on:
-// PHP-NUKE Web Portal System - http://phpnuke.org/
-// Thatware - http://thatware.org/
-// ----------------------------------------------------------------------
-// LICENSE
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WIthOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
-// Original Author of file: Philip Fletcher
-// Purpose of file:  Table information for FormExpress module
-// ----------------------------------------------------------------------
+/**
+ * FormExpress : Build forms for Zikula through a web interface
+ *
+ * @copyright (c) 2002 Stutchbury Limited, 2010 Chris Candreva
+ * @Version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package FormExpress
+ *
+ *
+ * Origianally written by Philip Fletcher for PostNuke
+ * Updated for Zikula API by Christopher X. Candreva
+ *
+ * LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License (GPL)
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WIthOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * To read the license please visit http: *www.gnu.org/copyleft/gpl.html
+ * ----------------------------------------------------------------------
+ * Purpose of file:  Table information for FormExpress module
+ * ----------------------------------------------------------------------
+ */
 
 /**
  * This function is called internally by the core whenever the module is
@@ -34,116 +36,93 @@
 function FormExpress_pntables()
 {
     // Initialise table array
-    $pntable = array();
+    $table = array();
 
-    // Get the name for the FormExpress item table.  This is not necessary
-    // but helps in the following statements and keeps them readable
-    $FormExpress = pnConfigGetVar('prefix') . '_FormExpress';
+    $table['FormExpress'] = DBUtil::getLimitedTablename('FormExpress');
+    $table['FormExpress_column'] = array(
+        'form_id'    => 'fx_form_id',
+        'form_name'   => 'fx_form_name',
+        'description'   => 'fx_description',
+        'submit_action' => 'fx_submit_action',
+        'success_action' => 'fx_success_action',
+        'failure_action' => 'fx_failure_action',
+        'onload_action' => 'fx_onload_action',
+        'validation_action' => 'fx_validation_action',
+        'active'   => 'fx_active',
+        'language'   => 'fx_language',
+        'input_name_suffix'   => 'fx_input_name_suffix',
+        );
+    $table['FormExpress_column_def'] = array(
+        'form_id'    => 'I NOTNULL PRIMARY AUTOINCREMENT',
+        'form_name'   => 'C(50) NOTNULL',
+        'description'   => 'X2',
+        'submit_action' => 'X2',
+        'success_action' => 'X2',
+        'failure_action' => 'X2',
+        'onload_action' => 'X2',
+        'validation_action' => 'X2',
+        'active'   => 'L NOTNULL',
+        'language'   => 'C(30) NOTNULL',
+        'input_name_suffix'   => 'C(30) NOTNULL',
+        );
 
-    // Set the table name
-    $pntable['FormExpress'] = $FormExpress;
 
-    // Set the column names.  Note that the array has been formatted
-    // on-screen to be very easy to read by a user.
-    $pntable['FormExpress_column'] = array( 'form_id'    => $FormExpress . '.fx_form_id'
-                                          , 'form_name'   => $FormExpress . '.fx_form_name'
-                                          , 'description'   => $FormExpress . '.fx_description'
-                                          , 'submit_action' => $FormExpress . '.fx_submit_action'
-                                          , 'success_action' => $FormExpress . '.fx_success_action'
-                                          , 'failure_action' => $FormExpress . '.fx_failure_action'
-                                          , 'onload_action' => $FormExpress . '.fx_onload_action'
-                                          , 'validation_action' => $FormExpress . '.fx_validation_action'
-                                          //, 'action_source' => $FormExpress . '.fx_action_source'
-                                          //, 'action_name' => $FormExpress . '.fx_action_name'
-                                          //, 'action_args' => $FormExpress . '.fx_action_args'
-                                          //, 'success_message' => $FormExpress . '.fx_success_message'
-                                          //, 'failure_message' => $FormExpress . '.fx_failure_message'
-                                          , 'active'   => $FormExpress . '.fx_active'
-                                          , 'language'   => $FormExpress . '.fx_language'
-                                          , 'input_name_suffix'   => $FormExpress . '.fx_input_name_suffix'
-                                          );
+    $table['FormExpressItem'] = DBUtil::getLimitedTablename('FormExpressItem');
+    $table['FormExpressItem_column'] = array(
+        'form_item_id'    => 'fx_form_item_id',
+        'form_id'   => 'fx_form_id',
+        'sequence'   => 'fx_sequence',
+        'item_type'   => 'fx_item_type',
+        'item_name'   => 'fx_item_name',
+        'required'   => 'fx_required',
+        'prompt'   => 'fx_prompt',
+        'prompt_position'   => 'fx_prompt_position',
+        'item_value'   => 'fx_item_value',
+        'default_value'   => 'fx_default_value',
+        'cols'   => 'fx_cols',
+        'rows'   => 'fx_rows',
+        'max_length'   => 'fx_max_length',
+        'item_attributes'   => 'fx_item_attributes',
+        'multiple'   => 'fx_multiple',
+        'validation_rule'   => 'fx_validation_rule',
+        'active'   => 'fx_active',
+        'relative_position'   => 'fx_relative_position',
+        );
+    $table['FormExpressItem_column_def'] = array(
+        'form_item_id'    => 'I NOTNULL PRIMARY AUTOINCREMENT',
+        'form_id'   => 'I NOTNULL',
+        'sequence'   => 'I NOTNULL',
+        'item_type'   => 'C(15) NOTNULL',
+        'item_name'   => 'C(30) NOTNULL',
+        'required'   => 'L NOTNULL',
+        'prompt'   => "C(100)",
+        'prompt_position'   => "C(10) DEFAULT 'left'",
+        'item_value'   => 'X2',
+        'default_value'   => 'C(100)',
+        'cols'   => 'I',
+        'rows'   => 'I',
+        'max_length'   => 'I',
+        'item_attributes'   => 'C(250)',
+        'multiple'   => 'L',
+        'validation_rule'   => 'C(250)',
+        'active'   => 'L NOTNULL',
+        'relative_position'   => "C(10) NOTNULL DEFAULT 'below'",
+        );
 
-    $FormExpressItem = pnConfigGetVar('prefix') . '_FormExpressItems';
-
-    // Set the table name
-    $pntable['FormExpressItem'] = $FormExpressItem;
-
-    // Set the column names.  Note that the array has been formatted
-    // on-screen to be very easy to read by a user.
-    $pntable['FormExpressItem_column'] = array( 'form_item_id'    => $FormExpressItem . '.fx_form_item_id'
-                                               , 'form_id'   => $FormExpressItem . '.fx_form_id'
-                                               , 'sequence'   => $FormExpressItem . '.fx_sequence'
-                                               , 'item_type'   => $FormExpressItem . '.fx_item_type'
-                                               , 'item_name'   => $FormExpressItem . '.fx_item_name'
-                                               , 'required'   => $FormExpressItem . '.fx_required'
-                                               , 'prompt'   => $FormExpressItem . '.fx_prompt'
-                                               , 'prompt_position'   => $FormExpressItem . '.fx_prompt_position'
-                                               , 'item_value'   => $FormExpressItem . '.fx_item_value'
-                                               , 'default_value'   => $FormExpressItem . '.fx_default_value'
-                                               , 'cols'   => $FormExpressItem . '.fx_cols'
-                                               , 'rows'   => $FormExpressItem . '.fx_rows'
-                                               , 'max_length'   => $FormExpressItem . '.fx_max_length'
-                                               , 'item_attributes'   => $FormExpressItem . '.fx_item_attributes'
-                                               , 'multiple'   => $FormExpressItem . '.fx_multiple'
-                                               , 'validation_rule'   => $FormExpressItem . '.fx_validation_rule'
-                                               , 'active'   => $FormExpressItem . '.fx_active'
-                                               , 'relative_position'   => $FormExpressItem . '.fx_relative_position'
-                                              );
-
-    // Set the table name
-    $FormExpressCache = pnConfigGetVar('prefix') . '_FormExpressCache';
-    $pntable['FormExpressCache'] = $FormExpressCache;
-
-    // Set the column names.  Note that the array has been formatted
-    // on-screen to be very easy to read by a user.
-    $pntable['FormExpressCache_column'] = array( 'form_id'   => $FormExpressCache . '.fx_form_id'
-                                               , 'form_data'   => $FormExpressCache . '.fx_form_data'
-                                               );
+    $table['FormExpressCache'] = DBUtil::getLimitedTablename('FormExpressCache');
+    $table['FormExpressCache_column'] = array(
+        'form_id'   => 'fx_form_id',
+        'form_data'   => 'fx_form_data'
+        );
+    $table['FormExpressCache_column_def'] = array(
+        'form_id'   => 'I NOTNULL PRIMARY AUTOINCREMENT',
+        'form_data'   => 'B DEFAULT NULL'
+        );
 
 
 
     // Return the table information
-    return $pntable;
+    return $table;
 }
-/*
-For cut & pasting...
-form_item_id
-form_id
-sequence
-item_type
-item_name
-required
-prompt
-prompt_position
-item_value
-default_value
-cols
-rows
-max_length
-item_attributes
-multiple
-group_title
-relative_position
-active
 
-
-form_item_id form_item_id
-form_id form_id
-sequence sequence
-item_type item_type
-item_name item_name
-required required
-prompt prompt
-prompt_position prompt_position
-item_value item_value
-default_value default_value
-cols cols
-rows rows
-max_length max_length
-item_attributes item_attributes
-multiple multiple
-group_title group_title
-relative_position relative_position
-active active
-*/
 ?>

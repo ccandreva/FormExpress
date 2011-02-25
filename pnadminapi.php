dd0dfcf0797784438ceab740739859936b25879a
Christopher X. Candreva <chris@westnet.com>
2011-02-25 21:17:38 +0000

    Update header comment block.

--- pnadminapi.php
+++ pnadminapi.php
@@ -1,31 +1,35 @@
 <?php
-// $Id$
-// ----------------------------------------------------------------------
-// FormExpress module for POST-NUKE Content Management System
-// Copyright (C) 2002 by Stutchbury Limited
-// http://www.stutchbury.net/
-// ----------------------------------------------------------------------
-// Based on:
-// PHP-NUKE Web Portal System - http://phpnuke.org/
-// Thatware - http://thatware.org/
-// ----------------------------------------------------------------------
-// LICENSE
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WIthOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
-// Original Author of file: Philip Fletcher
-// Purpose of file:  FormExpress administration API
-// ----------------------------------------------------------------------
+/**
+ * FormExpress : Build forms for Zikula through a web interface
+ *
+ * @copyright (c) 2002 Stutchbury Limited, 2010 Chris Candreva
+ * @Version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package FormExpress
+ *
+ *
+ * Origianally written by Philip Fletcher for PostNuke
+ * Updated for Zikula API by Christopher X. Candreva
+ *
+ * LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License (GPL)
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WIthOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * To read the license please visit http: *www.gnu.org/copyleft/gpl.html
+ * ----------------------------------------------------------------------
+ * Purpose of file:  FormExpress administration API
+ * ----------------------------------------------------------------------
+ */
+
+
 
 
 /**

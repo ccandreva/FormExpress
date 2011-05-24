20577516842f75f10baf01960962dfca9fd5ee18
Christopher X. Candreva <chris@westnet.com>
2011-05-24 22:42:56 +0000

    Convert pninit to DBUtil

--- pninit.php
+++ pninit.php
@@ -36,115 +36,15 @@
  */
 function FormExpress_init()
 {
-    pnModSetVar('FormExpress', 'modulestylesheet', 'style.css');
-    list($dbconn) = pnDBGetConn();
-    $pntable = pnDBGetTables();
-
-    // It's good practice to name the table and column definitions you
-    // are getting - $table and $column don't cut it in more complex
-    // modules
-    $FormExpresstable = $pntable['FormExpress'];
-    $FormExpresscolumn = &$pntable['FormExpress_column'];
-
-    // Create the table - the formatting here is not mandatory, but it does
-    // make the SQL statement relatively easy to read.  Also, separating out
-    // the SQL statement from the Execute() command allows for simpler
-    // debug operation if it is ever needed
-    $sql = "CREATE TABLE $FormExpresstable 
-              ( $FormExpresscolumn[form_id] int(10) NOT NULL auto_increment
-              , $FormExpresscolumn[form_name] varchar(50) NOT NULL
-              , $FormExpresscolumn[description] text
-              , $FormExpresscolumn[submit_action] text
-              , $FormExpresscolumn[success_action] text
-              , $FormExpresscolumn[failure_action] text
-              , $FormExpresscolumn[onload_action] text
-              , $FormExpresscolumn[validation_action] text
-              , $FormExpresscolumn[active] tinyint(4) NOT NULL default 0
-              , $FormExpresscolumn[language] varchar(30) NOT NULL
-              , $FormExpresscolumn[input_name_suffix] varchar(30) NOT NULL
-              , PRIMARY KEY( fx_form_id )
-              )";
-              //, PRIMARY KEY(fx_form_id)
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        pnSessionSetVar('errormsg', _CREATETABLEFAILED . $sql);
-        return false;
-    }
-
-    // It's good practice to name the table and column definitions you
-    // are getting - $table and $column don't cut it in more complex
-    // modules
-    $FormExpressItemtable = $pntable['FormExpressItem'];
-    $FormExpressItemcolumn = &$pntable['FormExpressItem_column'];
-
-    // Create the table - the formatting here is not mandatory, but it does
-    // make the SQL statement relatively easy to read.  Also, separating out
-    // the SQL statement from the Execute() command allows for simpler
-    // debug operation if it is ever needed
-    $sql = "CREATE TABLE $FormExpressItemtable 
-              ( $FormExpressItemcolumn[form_item_id] int(11) NOT NULL auto_increment
-              , $FormExpressItemcolumn[form_id] int(11) NOT NULL
-              , $FormExpressItemcolumn[sequence] int(11) NOT NULL
-              , $FormExpressItemcolumn[item_type] varchar(15) NOT NULL default ''
-              , $FormExpressItemcolumn[item_name] varchar(30) NOT NULL default ''
-              , $FormExpressItemcolumn[required] tinyint(4) NOT NULL default 0
-              , $FormExpressItemcolumn[prompt] varchar(100)
-              , $FormExpressItemcolumn[prompt_position] varchar(10) default 'left'
-              , $FormExpressItemcolumn[item_value] text
-              , $FormExpressItemcolumn[default_value] varchar(100)
-              , $FormExpressItemcolumn[cols] int(11)
-              , $FormExpressItemcolumn[rows] int(11)
-              , $FormExpressItemcolumn[max_length] int(11)
-              , $FormExpressItemcolumn[multiple] tinyint(4)
-              , $FormExpressItemcolumn[item_attributes] varchar(250)
-              , $FormExpressItemcolumn[validation_rule] varchar(250)
-              , $FormExpressItemcolumn[active] tinyint(4) NOT NULL default 0
-              , $FormExpressItemcolumn[relative_position] varchar(10) NOT NULL default 'below'
-              , PRIMARY KEY(fx_form_item_id)
-              )";
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        pnSessionSetVar('errormsg', _CREATETABLEFAILED . $sql);
-        return false;
-    }
-
-    //Create the cache table
-    $FormExpressCachetable = $pntable['FormExpressCache'];
-    $FormExpressCachecolumn = &$pntable['FormExpressCache_column'];
-
-    // Create the table - the formatting here is not mandatory, but it does
-    // make the SQL statement relatively easy to read.  Also, separating out
-    // the SQL statement from the Execute() command allows for simpler
-    // debug operation if it is ever needed
-    $sql = "CREATE TABLE $FormExpressCachetable
-              ( $FormExpressCachecolumn[form_id] int(11) NOT NULL
-              , $FormExpressCachecolumn[form_data] blob default null
-              , PRIMARY KEY(fx_form_id)
-              )";
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        pnSessionSetVar('errormsg', _CREATETABLEFAILED . $sql);
-        return false;
-    }
-
-
-
-    // Set up an initial value for a module variable.  Note that all module
-    // variables should be initialised with some value in this way rather
-    // than just left blank, this helps the user-side code and means that
-    // there doesn't need to be a check to see if the variable is set in
-    // the rest of the code as it always will be
+    
+    if ( !DBUtil::createTable('FormExpress') ) return false;
+    if ( !DBUtil::createTable('FormExpressItem') ) return false;
+    if ( !DBUtil::createTable('FormExpressCache') ) return false;
+    
+    // Initialize module variables
     pnModSetVar('FormExpress', 'default_form_id', -1);
     pnModSetVar('FormExpress', 'allow_dynamic_syntax', 1);
+    pnModSetVar('FormExpress', 'modulestylesheet', 'style.css');
     //pnModSetVar('FormExpress', 'itemsperpage', 10);
 
 /* Can't load the module since it isn't active yet.
@@ -173,24 +73,15 @@ function FormExpress_upgrade($oldversion)
     //This is actually the new version number after the module list has been regenerated!!!!
     switch($oldversion) { 
         case '0.3.0':
-            list($dbconn) = pnDBGetConn();
+
+            // Update table structures
+            if ( !DBUtil::changeTable('FormExpress') ) return false;
+            if ( !DBUtil::changeTable('FormExpressItem') ) return false;
+
             $pntable = pnDBGetTables();
             $FormExpresstable = $pntable['FormExpress'];
             $FormExpresscolumn = &$pntable['FormExpress_column'];
-            $FormExpressItemtable = $pntable['FormExpressItem'];
-            $FormExpressItemcolumn = &$pntable['FormExpressItem_column'];
-            $sql = "ALTER TABLE $FormExpresstable
-                          ADD fx_submit_action TEXT AFTER fx_description
-                        , ADD fx_success_action TEXT AFTER fx_submit_action
-                        , ADD fx_failure_action TEXT AFTER fx_success_action
-                        , ADD fx_onload_action TEXT AFTER fx_failure_action
-                        , ADD fx_validation_action TEXT AFTER fx_onload_action
-                        ";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
+
             //Populate the new columns
             $sql = "UPDATE $FormExpresstable
                        SET fx_submit_action = 
@@ -209,131 +100,16 @@ function FormExpress_upgrade($oldversion)
                                  , '" .pnVarPrepForStore( "' )] ") ." '
                                  )
                     ";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
-            // Drop non-required columns
-            $sql = "ALTER TABLE $FormExpresstable DROP fx_action_source";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
-            $sql = "ALTER TABLE $FormExpresstable DROP fx_action_name";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
-            $sql = "ALTER TABLE $FormExpresstable DROP fx_action_args";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
-            $sql = "ALTER TABLE $FormExpresstable DROP fx_success_message";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
-            $sql = "ALTER TABLE $FormExpresstable DROP fx_failure_message";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
-            //Modify the items table
-            $sql = "ALTER TABLE $FormExpressItemtable
-                        MODIFY fx_prompt varchar(250)
-                      , ADD fx_validation_rule varchar(250) AFTER fx_multiple
-                      , CHANGE 'fx_class_name' 'fx_item_attributes' varchar(250)
-                   ";
-            $dbconn->Execute($sql);
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED . $sql);
-                return false;
-            }
+            $result = DBUtil::executeSQL($sql);
 
             //Create the cache table
-            $FormExpressCachetable = $pntable['FormExpressCache'];
-            $FormExpressCachecolumn = &$pntable['FormExpressCache_column'];
-
-            // Create the table - the formatting here is not mandatory, but it does
-            // make the SQL statement relatively easy to read.  Also, separating out
-            // the SQL statement from the Execute() command allows for simpler
-            // debug operation if it is ever needed
-            $sql = "CREATE TABLE $FormExpressCachetable
-                      ( $FormExpressCachecolumn[form_id] int(11) NOT NULL
-                      , $FormExpressCachecolumn[form_data] blob default null
-                      , PRIMARY KEY(fx_form_id)
-                      )";
-            $dbconn->Execute($sql);
-
-            // Check for an error with the database code, and if so set an
-            // appropriate error message and return
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _CREATETABLEFAILED . $sql);
-                return false;
-            }
-
+            if ( !DBUtil::createTable('FormExpressCache') ) return false;
 
             pnModSetVar('FormExpress', 'default_form_id', 1);
             pnModSetVar('FormExpress', 'allow_dynamic_syntax', 1);
-        break;
-/*
-        case 0.5:
-            // Version 0.5 didn't have a 'number' field, it was added
-            // in version 1.0
+            pnModSetVar('FormExpress', 'modulestylesheet', 'style.css');
 
-            // Get datbase setup - note that both pnDBGetConn() and pnDBGetTables()
-            // return arrays but we handle them differently.  For pnDBGetConn()
-            // we currently just want the first item, which is the official
-            // database handle.  For pnDBGetTables() we want to keep the entire
-            // tables array together for easy reference later on
-            // This code could be moved outside of the switch statement if
-            // multiple upgrades need it
-            list($dbconn) = pnDBGetConn();
-            $pntable = pnDBGetTables();
-
-            // It's good practice to name the table and column definitions you
-            // are getting - $table and $column don't cut it in more complex
-            // modules
-            // This code could be moved outside of the switch statement if
-            // multiple upgrades need it
-            $FormExpresstable = $pntable['FormExpress'];
-            $FormExpresscolumn = &$pntable['FormExpress_column'];
-
-            // Add a column to the table - the formatting here is not
-            // mandatory, but it does make the SQL statement relatively easy
-            // to read.  Also, separating out the SQL statement from the
-            // Execute() command allows for simpler debug operation if it is
-            // ever needed
-            $sql = "ALTER TABLE $FormExpresstable
-                    ADD $FormExpresscolumn[number] int(5) NOT NULL default 0";
-            $dbconn->Execute($sql);
-
-            // Check for an error with the database code, and if so set an
-            // appropriate error message and return
-            if ($dbconn->ErrorNo() != 0) {
-                pnSessionSetVar('errormsg', _UPDATETABLEFAILED);
-                return false;
-            }
-
-            // At the end of the successful completion of this function we
-            // recurse the upgrade to handle any other upgrades that need
-            // to be done.  This allows us to upgrade from any version to
-            // the current version with ease
-            return FormExpress_upgrade(1.0);
-        case 1.0:
-            // Code to upgrade from version 1.0 goes here
             break;
-        case 2.0:
-            // Code to upgrade from version 2.0 goes here
-            break;
-*/
     }
 
     // Update successful
@@ -347,56 +123,14 @@ function FormExpress_upgrade($oldversion)
  */
 function FormExpress_delete()
 {
-    // Get datbase setup - note that both pnDBGetConn() and pnDBGetTables()
-    // return arrays but we handle them differently.  For pnDBGetConn()
-    // we currently just want the first item, which is the official
-    // database handle.  For pnDBGetTables() we want to keep the entire
-    // tables array together for easy reference later on
-    list($dbconn) = pnDBGetConn();
-    $pntable = pnDBGetTables();
-
-    // Drop the table - for such a simple command the advantages of separating
-    // out the SQL statement from the Execute() command are minimal, but as
-    // this has been done elsewhere it makes sense to stick to a single method
-    $sql = "DROP TABLE $pntable[FormExpress]";
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        // Report failed deletion attempt
-        return false;
-    }
-
-    // Drop the table - for such a simple command the advantages of separating
-    // out the SQL statement from the Execute() command are minimal, but as
-    // this has been done elsewhere it makes sense to stick to a single method
-    $sql = "DROP TABLE $pntable[FormExpressItem]";
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        // Report failed deletion attempt
-        return false;
-    }
-
-    // Drop the table - for such a simple command the advantages of separating
-    // out the SQL statement from the Execute() command are minimal, but as
-    // this has been done elsewhere it makes sense to stick to a single method
-    $sql = "DROP TABLE $pntable[FormExpressCache]";
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        // Report failed deletion attempt
-        return false;
-    }
+    DBUtil::dropTable('FormExpress');
+    DBUtil::dropTable('FormExpressItem');
+    DBUtil::dropTable('FormExpressCache');
 
     // Delete any module variables
     pnModDelVar('FormExpress', 'default_form_id', 1);
     pnModDelVar('FormExpress', 'allow_dynamic_syntax', 1);
+    pnModDelVar('FormExpress', 'modulestylesheet', 'style.css');
 
     // Deletion successful
     return true;
